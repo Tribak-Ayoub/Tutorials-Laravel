cac45a94b47d7409dbdcb47a0223f2faafa6ee8c
Tribak77 <atribak34@gmail.com>
2024-10-31 10:33:22 +0000

    update the command file of create category

--- Eloquent ORM/Prototype/app/Console/Commands/CreateCategory.php
+++ Eloquent ORM/Prototype/app/Console/Commands/CreateCategory.php
@@ -3,28 +3,23 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Category;
 
 class CreateCategory extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:create-category';
+    protected $signature = 'create:category {name}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'Create a new category';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $name = $this->argument('name');
+
+        $category = Category::create(['name' => $name]);
+
+        $this->info("Category '{$name}' created successfully!");
     }
 }
