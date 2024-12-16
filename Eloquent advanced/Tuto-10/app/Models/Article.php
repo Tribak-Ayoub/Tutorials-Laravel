18c3ddd3d9232df9aee0eed4eac76d8d5414d15d
Ayoub Tribak <tribak.elyedri.ayoub@gmail.com>
2024-12-17 00:04:21 +0100

    save

--- Eloquent advanced/Tuto-10/app/Models/Article.php
+++ Eloquent advanced/Tuto-10/app/Models/Article.php
@@ -6,7 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    public function user() {
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'category_id',
+    ];
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
