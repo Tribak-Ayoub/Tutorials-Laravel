019fd3331a5bbb7d37f9f0f81eb87d99c5639532
Tribak77 <atribak34@gmail.com>
2024-12-17 13:01:11 +0000

    add the profile table and add the relations

--- Eloquent advanced/Tuto-10/app/Models/User.php
+++ Eloquent advanced/Tuto-10/app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Article::class);
     }
+
+    public function profile() {
+        return $this->hasOne(Profile::class);
+    }
 }
