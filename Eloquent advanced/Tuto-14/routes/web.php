384995aa0d2fcadff42a793f21bb3dcae3ab27c4
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2024-12-20 12:46:47 +0000

    save

--- Eloquent advanced/Tuto-14/routes/web.php
+++ Eloquent advanced/Tuto-14/routes/web.php
@@ -17,3 +17,11 @@ Route::get('/dashboard', function () {
 })->middleware('auth');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', function () {
+    return 'Admin Dashboard';
+})->middleware('role:admin');
+
+Route::get('/editor', function () {
+    return 'Editor Dashboard';
+})->middleware('role:editor');
