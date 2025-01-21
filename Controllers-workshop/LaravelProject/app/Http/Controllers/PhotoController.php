0add3ea5fe630906fba5b78d846800947d0169d5
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-01-21 23:07:42 +0000

    save

---
+++ Controllers-workshop/LaravelProject/app/Http/Controllers/PhotoController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photo;
+use App\Http\Requests\StorePhotoRequest;
+use App\Http\Requests\UpdatePhotoRequest;
+use Illuminate\Routing\Controllers\HasMiddleware;
+
+class PhotoController extends Controller
+{
+    use HasMiddleware;
+
+        // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    protected array $middleware = [
+        'auth', // Apply 'auth' middleware to all methods
+    ];
+
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePhotoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePhotoRequest $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+}
--- Controllers-workshop/LaravelProject/routes/web.php
+++ Controllers-workshop/LaravelProject/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('photos', PhotoController::class)->only(['index', 'create']);
+
+Route::resource('photos', PhotoController::class)->except(['store', 'update']);
